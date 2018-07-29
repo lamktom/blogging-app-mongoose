e227bfb5f2226139d60c6181785e3321ee5e64b3
Tom Lam <lamktom@yahoo.com>
2018-07-29 11:09:37 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -32,65 +32,91 @@ app.get('/posts', (req, res) => {
 app.get('/posts/:id', (req,res) => {
 	BlogPost
 		.findById(req.params.id)
-		.then(post => res.json(posts.serialize()));
+		.then(post => res.json(post.serialize()));
 		.catch(err => {
 			console.log(err);
 			res.status(500).json({ error: 'Internal server error' }); 
 		});
 }); 
 
-// post endpoint 
 app.post('/posts', (req, res) => {
-	const requiredFields = ['title', 'content', 'author'];
-	for (let i = 0; i < requiredFields.length; i++) {
-		const field = requiredFields[i];
-		if (!(field in req.body)) {
-			const message = `Missing \`${field}\` in request body`; 
-			console.error(message); 
-			return res.status(400).send(message); 
-		}
-	}
-}); 
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
 
-// put endpoint 
-app.put('/posts/:id', (req, res) => {
-	if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-		res.status(400).json({
-			error: 'Request path id and request body id values must match'
-		});
-	}
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(blogPost => res.status(201).json(blogPost.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
 
-	const updated = {};
-	const updateableFields = ['title', 'content', 'author'];
-	updateableFields.forEach(field => {
-		if (field in req.body) {
-			updated[field] = req.body[field]; 
-		}
-	});
+});
 
-	BlogPost
-		.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-		.then(updatedPost => res.status(204).end())
-		.catch(err => res.status(500).json({ message: 'Something went wrong' }));
-}); 
-
-// delete endpoint 
 app.delete('/posts/:id', (req, res) => {
-	BlogPost
-		.findByIdAndRemove(req.params.id)
-		.then(() => {
-			console.log(`Deleted blog post with id \`${req.params.id}\``)
-			// respond with 204 (success, no content returned) 
-			res.status(204).end(); 
-		});
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
 });
 
-// catch-all endpoint just in case client makes requests to endpoint that doesn't exist
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  BlogPost
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+});
+
+
+app.delete('/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``);
+      res.status(204).end();
+    });
+});
+
+
 app.use('*', function (req, res) {
-	res.status(404).json({ message: "Not Found"}); 
+  res.status(404).json({ message: 'Not Found' });
 });
 
-let server; 
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
 
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
@@ -134,18 +160,3 @@ if (require.main === module) {
 }
 
 module.exports = { runServer, app, closeServer };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
